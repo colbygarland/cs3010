eb61efe28cc36ac15c04dcc57d8b79f01a138880
Colby Garland <colbygarland@gmail.com>
2015-01-26 18:10:43 -0700

    added nice things

--- lab3/src/lab3/Lab3.java
+++ lab3/src/lab3/Lab3.java
@@ -22,6 +22,8 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
@@ -30,60 +32,63 @@ import javafx.util.Duration;
  * @author francoc
  */
 public class Lab3 extends Application {
-    
-      /*
-        Image image = new Image("file:" + pictures[0]);
-        imgView.setImage(image);
-      */
+
     int index = 0;
-    
+
     @Override
     public void start(Stage primaryStage) {
-        
-         class JPGFilter implements FileFilter{
+
+        class JPGFilter implements FileFilter {
+
             @Override
             public boolean accept(File pathname) {
                 String name = pathname.getName();
-                if (pathname.isDirectory()) return false;
-                if(name==null)return false;
-                return name.toLowerCase().endsWith(".jpg") || 
-                       name.toLowerCase().endsWith(".png") ||
-                       name.toLowerCase().endsWith(".gif");
+                if (pathname.isDirectory()) {
+                    return false;
+                }
+                if (name == null) {
+                    return false;
+                }
+                return name.toLowerCase().endsWith(".jpg")
+                        || name.toLowerCase().endsWith(".png")
+                        || name.toLowerCase().endsWith(".gif");
             }
         }
-        
+
         File f = new File(".");
-        File [] pictures = f.listFiles(new JPGFilter());
+        File[] pictures = f.listFiles(new JPGFilter());
         int NUMBER_OF_PICTURES = pictures.length;
-        
+
         // image stuff here
         ImageView imgView = new ImageView();
         imgView.setFitHeight(700);
         imgView.setFitWidth(500);
         imgView.setPreserveRatio(true);
-        
+
         // text stuff for bottom corner
         Text text = new Text();
         Font font = new Font(20);
-        
-        
+
 // BUTTON HANDLING 
-        
-    // previous button
+        // previous button
         Button btnPrev = new Button();
         btnPrev.setText("Prev");
         btnPrev.setFont(font);
+        btnPrev.setStyle("-fx-font: 22 Arial; -fx-base: #d3d3d3;");
         btnPrev.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
-                if (index > 0) index--;
-                else index = NUMBER_OF_PICTURES - 1;
+                if (index > 0) {
+                    index--;
+                } else {
+                    index = NUMBER_OF_PICTURES - 1;
+                }
                 Image img = new Image("file:" + pictures[index]);
                 imgView.setImage(img);
                 String str = pictures[index].getName();
                 text.setText(str);
-                
+
                 FadeTransition ft = new FadeTransition(Duration.millis(500), imgView);
                 ft.setFromValue(0.3);
                 ft.setToValue(1.0);
@@ -92,34 +97,40 @@ public class Lab3 extends Application {
                 ft.play();
             }
         });
-        
+
         btnPrev.setOnMouseEntered(e -> {
-            btnPrev.setTextFill(Color.CORAL);
-            DropShadow shadow = new DropShadow();
-            btnPrev.setEffect(shadow);
+            btnPrev.setTextFill(Color.BLUE);
+            DropShadow dropShadow = new DropShadow();
+            dropShadow.setOffsetX(5);
+            dropShadow.setOffsetY(5);
+            dropShadow.setRadius(5);
+            dropShadow.setColor(Color.color(0, 0, 0, 0.45));
+            btnPrev.setEffect(dropShadow);
         });
-        
+
         btnPrev.setOnMouseExited(e -> {
             btnPrev.setTextFill(Color.BLACK);
             btnPrev.setEffect(null);
         });
-                
-                
-    // next button
+
+        // next button
         Button btnNext = new Button();
         btnNext.setText("Next");
-        btnNext.setFont(font);
+        btnNext.setStyle("-fx-font: 22 jokerman; -fx-base: #d3d3d3;");
         btnNext.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
-                if (index < NUMBER_OF_PICTURES - 1) index++;
-                else index = 0;
+                if (index < NUMBER_OF_PICTURES - 1) {
+                    index++;
+                } else {
+                    index = 0;
+                }
                 Image img = new Image("file:" + pictures[index]);
                 imgView.setImage(img);
                 String str = pictures[index].getName();
                 text.setText(str);
-                
+
                 FadeTransition ft = new FadeTransition(Duration.millis(500), imgView);
                 ft.setFromValue(0.3);
                 ft.setToValue(1.0);
@@ -128,71 +139,78 @@ public class Lab3 extends Application {
                 ft.play();
             }
         });
-        
-         btnNext.setOnMouseEntered(e -> {
-            btnNext.setTextFill(Color.CORAL);
-            DropShadow shadow = new DropShadow();
-            btnNext.setEffect(shadow);
+
+        btnNext.setOnMouseEntered(e -> {
+            btnNext.setTextFill(Color.BLUE);
+            DropShadow dropShadow = new DropShadow();
+            dropShadow.setOffsetX(-5);
+            dropShadow.setOffsetY(5);
+            dropShadow.setRadius(5);
+            dropShadow.setColor(Color.color(0, 0, 0, 0.45));
+            btnNext.setEffect(dropShadow);
         });
-        
+
         btnNext.setOnMouseExited(e -> {
             btnNext.setTextFill(Color.BLACK);
             btnNext.setEffect(null);
         });
-        
-        
+
         BorderPane borderPane = new BorderPane();
-        
+
         borderPane.setLeft(btnPrev);
         borderPane.setRight(btnNext);
-        
+
         Image begin = new Image("file:" + pictures[0]);
         imgView.setImage(begin);
         borderPane.setCenter(imgView);
         String str = pictures[0].getName();
         text.setText(str);
         borderPane.setBottom(text);
-        
+
         borderPane.setOnKeyPressed((KeyEvent ke) -> {
-            if (ke.getCode() == KeyCode.RIGHT || ke.getCode() == KeyCode.D){
-                if (index < NUMBER_OF_PICTURES - 1) index++;
-                    else index = 0;
-                    Image img = new Image("file:" + pictures[index]);
-                    imgView.setImage(img);
-                    String str1 = pictures[index].getName();
-                    text.setText(str1);
-                
-                    FadeTransition ft = new FadeTransition(Duration.millis(500), imgView);
-                    ft.setFromValue(0.3);
-                    ft.setToValue(1.0);
-                    ft.setCycleCount(1);
-                    ft.setAutoReverse(true);
-                    ft.play();
-            }
-            if (ke.getCode() == KeyCode.LEFT || ke.getCode() == KeyCode.S){
-                if (index > 0) index--;
-                else index = NUMBER_OF_PICTURES - 1;
+            if (ke.getCode() == KeyCode.RIGHT || ke.getCode() == KeyCode.D) {
+                if (index < NUMBER_OF_PICTURES - 1) {
+                    index++;
+                } else {
+                    index = 0;
+                }
                 Image img = new Image("file:" + pictures[index]);
                 imgView.setImage(img);
-                String str2 = pictures[index].getName();
-                text.setText(str2);
-                
+                String str1 = pictures[index].getName();
+                text.setText(str1);
+
                 FadeTransition ft = new FadeTransition(Duration.millis(500), imgView);
                 ft.setFromValue(0.3);
                 ft.setToValue(1.0);
                 ft.setCycleCount(1);
                 ft.setAutoReverse(true);
-                ft.play(); 
+                ft.play();
             }
-            if (ke.getCode() == KeyCode.ESCAPE){
+            if (ke.getCode() == KeyCode.LEFT || ke.getCode() == KeyCode.S) {
+                if (index > 0) {
+                    index--;
+                } else {
+                    index = NUMBER_OF_PICTURES - 1;
+                }
+                Image img = new Image("file:" + pictures[index]);
+                imgView.setImage(img);
+                String str2 = pictures[index].getName();
+                text.setText(str2);
+
+                FadeTransition ft = new FadeTransition(Duration.millis(500), imgView);
+                ft.setFromValue(0.3);
+                ft.setToValue(1.0);
+                ft.setCycleCount(1);
+                ft.setAutoReverse(true);
+                ft.play();
+            }
+            if (ke.getCode() == KeyCode.ESCAPE) {
                 System.exit(1);
             }
         });
-        
-        
+
         Scene scene = new Scene(borderPane, 800, 600);
-      
-        
+
         primaryStage.setTitle("Image Viewer");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -204,5 +222,5 @@ public class Lab3 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
