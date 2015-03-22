5818e850b16b22e2258d3b93ac5afca146dbc614
Colby Garland <colbygarland@gmail.com>
2015-03-21 20:38:06 -0600

    split up all the code, phew

---
+++ drawing/src/drawing/DrawPane.java
@@ -0,0 +1,243 @@
+/*
+All the code relating to the DrawPane class
+*/
+package drawing;
+
+import java.io.File;
+import java.util.Stack;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+class DrawPane extends Pane{
+    private MyShape selectedShape=null;
+    private boolean dragging = false;
+    private double oldMouseX;
+    private double oldMouseY;
+    private MyShape temp = null;
+    private ContextMenu contextmenu = new ContextMenu();
+    private boolean contextFlag = true;
+    private String capturedText = "";
+    private Stack stack;
+
+    public DrawPane(){
+        super();
+        stack = new Stack();
+        this.setPrefSize(800, 600);
+        this.setOnMousePressed(e->mousePressed(e));
+        this.setOnMouseReleased(e->mouseReleased(e));
+    }
+    public MyShape getSelectedShape(){
+        return selectedShape;
+    }
+    public MyShape [] getSelectedShapes(){ //This could be useful!
+      return null;  
+    }
+    public MyShape [] getUnSelectedShapes(){ //This could be useful too!
+      return null;  
+    } 
+    public void setZOrderingBack(MyShape s){
+        s.toBack();
+    }
+    public void setZOrderingFront(MyShape s){
+        s.toFront();
+    }
+    
+    protected void keyTyped(KeyEvent ke){
+        if (ke.getCode() == KeyCode.DELETE)
+            if (this.selectedShape != null) this.getChildren().remove(this.selectedShape);
+        if (ke.getCode() == KeyCode.C && this.selectedShape != null) 
+             this.copy(this.selectedShape);
+    }
+    private Node linePressed(MouseEvent me){
+        Line l = new Line();
+        l.setStartX(me.getSceneX()-50);
+        l.setStartY(me.getSceneY()-30);
+        l.setEndX(me.getSceneX() + 100);
+        l.setEndY(me.getSceneY());
+        Node n = l;
+        return n;
+    }
+    private void mousePressed(MouseEvent me){
+       
+        if (me.getButton() == MouseButton.SECONDARY) contextMenu(me);
+        else {
+            if (this.selectedShape == null){
+                MyShape s = new MyShape();
+                if(MyShape.getDefaultShapeType()==MyShape.LINE){
+                    
+                    // TODO: fix this so the line starts where the mouse is
+                    
+                    s.shape = linePressed(me);
+                } else if(MyShape.getDefaultShapeType()==MyShape.TEXT_BOX){
+                    Bounds boundsInParent = s.getBoundsInParent();
+                    double width = boundsInParent.getWidth();
+                    double height = boundsInParent.getHeight();
+                    s.relocate(me.getX()-s.getInsets().getLeft()-width/2,
+                       me.getY()-s.getInsets().getTop()-height/2);           
+                }
+                else {
+                    s.relocate(me.getX()-s.getInsets().getLeft()-MyShape.getDefaultWidth()/2, 
+                    me.getY()-s.getInsets().getTop()-MyShape.getDefaultHeight()/2); 
+                }
+                s.setOnMousePressed(e->shapePressed(e,s));
+                s.setOnMouseReleased(e->shapeReleased(e,s));
+                s.setOnMouseDragged(e->shapeDragged(e,s));
+                this.getChildren().add(s);
+            }
+        
+            // deselect everything 
+            contextmenu.hide();
+            if (!(this.getSelectedShape() == null)){
+                this.getSelectedShape().setSelected(false);
+                selectedShape = null;
+            }
+        }
+    }
+    
+    private void contextMenu(MouseEvent me){
+        if (!contextmenu.isShowing()){
+            if (contextFlag){
+                MenuItem copy = new MenuItem("Cut");
+                MenuItem paste = new MenuItem("Paste");
+                MenuItem undo = new MenuItem("Undo");
+                MenuItem redo = new MenuItem("Redo");
+                MenuItem delete = new MenuItem("Delete");
+                delete.setOnAction(e -> this.getChildren().remove(this.selectedShape));
+                copy.setOnAction(e -> this.copy(this.selectedShape));
+                paste.setOnAction(e -> this.paste(me));
+                undo.setOnAction(e -> undo());
+                redo.setOnAction(e -> redo());
+                contextmenu.getItems().addAll(copy,paste,undo,redo,delete);
+                contextFlag = false;
+            }
+            contextmenu.show(this, me.getScreenX(), me.getScreenY());
+        }
+    }
+    
+    private void mouseReleased(MouseEvent me){
+        //System.out.println("MouseReleased");
+        
+    }
+
+    private void shapePressed(MouseEvent e, MyShape s) {
+       // System.out.println("ShapePressed");
+        if(e.isSecondaryButtonDown()) contextMenu(e);
+        if(e.isPrimaryButtonDown() && !s.isSelected()){
+            s.setSelected(true);
+        }
+        if(s.isSelected()){
+            selectedShape = s;
+            oldMouseX = e.getSceneX();
+            oldMouseY = e.getSceneY();
+            //** See if this is a good idea
+            //MyShape.setDefaultShapeType(s.shapeType);   
+        } else {
+            selectedShape = null;
+        }
+        e.consume();//Don't trigger any clicks in the parent
+    }
+
+    private void shapeReleased(MouseEvent e, MyShape s) {
+        //System.out.println("ShapeReleased");
+        dragging=false;
+    }
+
+    private void shapeDragged(MouseEvent e, MyShape s) {
+        //System.out.println("ShapeDragged");
+        if(s.isSelected()) {
+            stack.push(s);
+            String shapeDraggedMsg = new String("ShapeDragged");
+            stack.push(shapeDraggedMsg);
+            
+            double newMouseX = e.getSceneX();
+            double newMouseY = e.getSceneY();
+            double dx = newMouseX-oldMouseX;
+            double dy = newMouseY-oldMouseY;
+            oldMouseX = newMouseX;
+            oldMouseY = newMouseY;
+            double centerX = s.getWidth() / 2 + 25;
+            double centerY = s.getHeight() / 2 + 25;
+            double x = newMouseX-centerX;
+            double y = newMouseY-centerY;
+            if(s.shapeContains(e.getX(),e.getY())||dragging){
+               dragging=true;
+               s.relocate(x/*+dx*/,y/*+dy*/);
+            }
+            else {
+               s.setPrefHeight(s.getHeight()+dy);
+               s.setPrefWidth(s.getWidth()+dx); 
+            }
+            if (s.shapeType == MyShape.LINE) s.changeLineSize(e, s);
+            else s.changeSizeButOnlyDuringADrag(s.getWidth(), s.getHeight());
+        }
+    }
+    
+    public void copy(MyShape s) {
+        if (this.selectedShape == null) return;
+        temp = new MyShape(s);
+    }
+    
+    public void paste(MouseEvent me){
+        if (!(temp == null)){
+            if (selectedShape == null){
+                MyShape s = new MyShape(temp);
+                s.relocate(/*me.getSceneX()-s.getInsets().getLeft()-MyShape.getDefaultWidth()/2*/me.getSceneX() ,
+                  me.getSceneY());
+                s.setOnMousePressed(e->shapePressed(e,s));
+                s.setOnMouseReleased(e->shapeReleased(e,s));
+                s.setOnMouseDragged(e->shapeDragged(e,s));
+                this.getChildren().add(s);
+            }
+        }
+    }
+    
+    public void undo(){
+        String tmp = (String)stack.pop();
+        
+        if (tmp.equals("ShapeDragged")){
+            MyShape ms = (MyShape)stack.pop();
+            
+            
+        }
+    }
+    
+    public void redo(){
+        
+    }
+
+    public void save(){
+        FileChooser saveChooser = new FileChooser();
+        saveChooser.setTitle("Save Drawing");
+        File f = saveChooser.showSaveDialog(new Stage());
+        
+        // locations for the nodes
+        double x,y;
+        // file to save to
+
+        
+        for (Node shape: this.getChildren()){
+            x = shape.getLayoutX();
+            y = shape.getLayoutY();
+            
+            
+        }
+    }
+    
+    public void open(){
+        FileChooser drawingChooser = new FileChooser();
+        drawingChooser.setTitle("Open Drawing");
+        File drawing = drawingChooser.showOpenDialog(new Stage());
+    }
+    
+
+}
---
+++ drawing/src/drawing/MyShape.java
@@ -0,0 +1,347 @@
+/*
+All the code relating to the MyShape class
+*/
+package drawing;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.collections.ObservableList;
+import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBoundsType;
+
+interface Drawable {
+    void setFillColor(Color c);
+    void setStrokeColor(Color c); 
+    void setStrokeWidth(int width);
+    void setFont(Font value);
+    void setText(String value);
+    Paint getFillColor();
+    Paint getStrokeColor(); 
+    double getStrokeWidth();
+    double [] getStrokeDashArray();    
+    Font getFont();
+    String getText();
+}
+
+class MyShape extends StackPane implements Drawable{
+    public static final int CIRCLE = 0;
+    public static final int RECTANGLE = 1;
+    public static final int ROUNDED_RECTANGLE = 2;
+    public static final int OVAL = 3;
+    public static final int TRIANGLE = 4;
+    public static final int LINE = 5;
+    public static final int SQUIGGLE = 6;
+    public static final int TEXT_BOX = 7;
+    public static final int PICTURE = 8;
+    private final SimpleBooleanProperty selected;
+    protected Node shape;
+    private static Paint defaultFillPaint=Color.RED;
+    protected static Paint defaultStrokePaint=Color.BLACK;
+    private static Paint defaultSelectedPaint=Color.BROWN;
+    private static double defaultStrokeWidth=3;
+    private static int defaultShapeType = CIRCLE;
+    private static double defaultWidth = 50;
+    private static double defaultHeight = 50;
+    protected int shapeType = 0;
+    private static String defaultFontName = "Times Roman";
+    private static double defaultFontSize = 15;
+
+    
+    private Node makeShape(){
+        Node s = null;
+        switch(defaultShapeType){
+            case CIRCLE: s = new Circle(Math.min(defaultWidth/2, defaultHeight/2)); shapeType = CIRCLE;break;
+            case RECTANGLE: s = new Rectangle(defaultWidth,defaultHeight);shapeType = RECTANGLE;break;
+            case ROUNDED_RECTANGLE: Rectangle r = new Rectangle(defaultWidth*2, defaultHeight);
+                                    r.setArcHeight(20);
+                                    r.setArcWidth(20);
+                                    s = r;
+                                    shapeType = ROUNDED_RECTANGLE;
+                                    break;
+            case OVAL:  Ellipse e = new Ellipse();
+                        e.setCenterX(defaultWidth/2);
+                        e.setCenterY(defaultHeight/2);
+                        e.setRadiusX(defaultWidth);
+                        e.setRadiusY(defaultHeight/2);
+                        s = e;
+                        shapeType = OVAL;
+                        break;
+            case TRIANGLE: s = new Polygon(defaultWidth*1.5,0,defaultWidth*2,defaultWidth,
+                    defaultWidth,defaultWidth); shapeType = TRIANGLE;
+                           break;
+            case LINE: Line l = new Line();
+                       l.setStartX(0);
+                       l.setStartY(0);
+                       l.setEndX(defaultWidth);
+                       l.setEndY(defaultHeight);
+                       s = l;
+                       shapeType = LINE;
+                       break;
+            case TEXT_BOX: Text t = new Text("Text");
+                           t.setStroke(defaultFillPaint);
+                           s = t;
+                           shapeType = TEXT_BOX;
+                            break;
+            case PICTURE: shapeType = PICTURE;break;
+            case SQUIGGLE: shapeType = SQUIGGLE;
+                           s = new Scribble();
+                           break;
+        }
+        return s;
+    }
+
+    public MyShape(){
+        super();
+        selected = new SimpleBooleanProperty(false);
+        selected.set(false);
+        shape = makeShape();
+        if (shape instanceof Text){
+           ((Text)shape).setStroke(defaultStrokePaint);
+           ((Text)shape).setFont(new Font(defaultFontName,defaultFontSize));
+           ((Text)shape).setBoundsType(TextBoundsType.VISUAL);
+        }
+        else if (shape instanceof Shape){
+            ((Shape)shape).setFill(defaultFillPaint);
+            ((Shape)shape).setStroke(defaultStrokePaint);
+            ((Shape)shape).setStrokeWidth(defaultStrokeWidth);
+        } 
+        this.getChildren().add(shape);
+        this.setPadding(new Insets(5,5,5,5));//A couple of magic numbers 5..10
+        this.setMinWidth(10);
+        this.setMinHeight(10);
+    }
+    
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+    
+    public MyShape(MyShape other){
+        super();
+        this.shape = other.shape;
+        this.selected = new SimpleBooleanProperty(false);
+        this.selected.set(false);
+        this.shapeType = other.shapeType;
+        setDefaultShapeType(this.shapeType);
+        this.makeShape();
+        if (this.shape instanceof Shape){
+            ((Shape)this.shape).setFill(defaultFillPaint);
+            ((Shape)this.shape).setStroke(defaultStrokePaint);
+            ((Shape)this.shape).setStrokeWidth(defaultStrokeWidth);
+        }
+        this.getChildren().add(shape);
+       // this.setPadding(new Insets(5,5,5,5));//A couple of magic numbers 5..10
+       // this.setMinWidth(10);
+        //this.setMinHeight(10);
+    }
+   
+    public void changeSizeButOnlyDuringADrag(double width, double height){ //Buggy
+        if(shape instanceof Circle){
+            Circle c = (Circle)shape; 
+            c.setRadius(Math.min(width/2.0,height/2.0)-this.getInsets().getLeft()-c.getStrokeWidth()/2.0);
+        } else if(shape instanceof Rectangle){
+            Rectangle r = (Rectangle)shape;
+            r.setWidth(width-this.getInsets().getLeft()-this.getInsets().getRight()-r.getStrokeWidth());
+            r.setHeight(height-this.getInsets().getTop()-this.getInsets().getBottom()-r.getStrokeWidth());
+        } else if(shape instanceof Ellipse){
+            Ellipse e = (Ellipse)shape;
+            e.setCenterX(width/2);
+            e.setCenterY(height);
+            e.setRadiusX(width/2.0 - this.getInsets().getLeft() - e.getStrokeWidth()/2.0);
+            e.setRadiusY(height/2.0 - this.getInsets().getTop() - this.getInsets().getBottom() - e.getStrokeWidth());
+        } else if(shape instanceof Polygon){
+            Polygon s = (Polygon)shape;
+            s.setScaleX(width/10.0 - this.getInsets().getLeft() - this.getInsets().getRight() - s.getStrokeWidth()/2.0);
+            s.setScaleY(height/10.0 - this.getInsets().getTop() - this.getInsets().getBottom() - s.getStrokeWidth()/2.0);
+        } else if(shape instanceof Text){
+            Text t = (Text)shape;
+            Bounds boundsInLocal = t.getBoundsInLocal();
+            double h = boundsInLocal.getHeight();
+            double w = boundsInLocal.getWidth();
+            double newHeight = height - getInsets().getTop() - getInsets().getBottom();
+            double newWidth = width - getInsets().getLeft() - getInsets().getRight();
+            double wr = newWidth/w;
+            double hr = newHeight/h;
+            double scale = Math.min(wr, hr);
+            double newSize = Math.max(t.getFont().getSize()*scale,2);
+            String name = t.getFont().getName();
+            t.setFont(new Font(name,newSize));   
+        } 
+    }
+    
+    public void changeLineSize(MouseEvent me, MyShape ms){
+        Line l = (Line)ms.shape;
+        
+        double x = me.getSceneX();
+        double y = me.getSceneY();
+        double endX = me.getX();
+        double endY = me.getY();
+        l.setStartX(x);
+        l.setStartY(y);
+        
+        l.setEndX(endX);
+        l.setEndY(endY);
+    }
+    
+    public boolean isSelected(){
+        return selected.get();
+    }
+    public void setSelected(boolean value){
+        selected.set(value);
+        if(value)this.setBackground(new Background(new BackgroundFill(defaultSelectedPaint,CornerRadii.EMPTY,Insets.EMPTY)));
+        else this.setBackground(Background.EMPTY);
+    }
+    public BooleanProperty selectedProperty(){
+        return selected;
+    } 
+    public boolean shapeContains(double x,  double y){
+        if (shape instanceof Circle) return shape.contains(x-this.getWidth()/2, y-this.getHeight()/2);
+        else if(shape instanceof Rectangle)
+            return shape.contains(x-this.getInsets().getLeft(), y-this.getInsets().getTop());
+        else if(shape instanceof Ellipse)
+            return shape.contains(x - this.getInsets().getLeft(), y-this.getInsets().getTop());
+        else if(shape instanceof Polygon)
+            return shape.contains(x - this.getInsets().getLeft(), y-this.getInsets().getTop());
+        else if(shape instanceof Line)
+            return shape.contains(x - this.getInsets().getLeft(), y-this.getInsets().getTop());
+        else if(shape instanceof Text){
+            Insets insets = this.getInsets();
+            return x>insets.getLeft()&& x < this.getWidth()-insets.getRight()&&y>insets.getTop()&&y<this.getHeight()-insets.getBottom();
+        }
+        else return false;
+    }
+    public static void setDefaultFillPaint(Paint value){
+        defaultFillPaint = value;
+    }
+    public static void setDefaultStrokePaint(Paint value){
+        defaultStrokePaint = value;
+    }
+    public static void setDefaultSelectedPaint(Paint value){
+        defaultSelectedPaint = value;
+    }
+    public static void setDefaultStrokeWidth(double value){
+        defaultStrokeWidth = value;
+    }
+    public static void setDefaultShapeType(int value){
+        defaultShapeType = value;
+    }
+    public static void setDefaultWidth(double value){
+        defaultWidth = value;
+    }
+    public static void setDefaultHeight(double value){
+        defaultHeight = value;
+    }
+    public static double getDefaultWidth(){
+        return defaultWidth;
+    }
+    public static double getDefaultHeight(){
+        return defaultHeight;
+    } 
+    public static int getDefaultShapeType(){
+        return defaultShapeType;
+    }
+    public void setFillColor(Color value){
+        if(shape instanceof Shape){
+            ((Shape)shape).setFill(value);
+        }
+    }
+    public void setStrokeColor(Color value){
+        if(shape instanceof Shape){
+            ((Shape)shape).setStroke(value);
+        }
+    }
+    public void setStrokeWidth(int value){
+        if(shape instanceof Shape){
+            ((Shape)shape).setStrokeWidth(value);
+        }
+    }
+    public void setStrokeDashArray(double [] value){
+        if(shape instanceof Shape){
+            ((Shape)shape).getStrokeDashArray().clear();
+            for(int i = 0; i < value.length; i++)
+               ((Shape)shape).getStrokeDashArray().add(value[i]);
+        }
+    }
+     @Override
+    public void setFont(Font value) {
+        if(shape instanceof Text){
+           ((Text)shape).setFont(value);
+        }
+    }
+    public void editText(Text t){
+        if (this.isSelected()) t.setOnMouseClicked(e -> {
+            String msg;
+            msg = this.getOnKeyTyped().toString();
+            t.setText(msg);
+        });
+    }
+
+@Override
+    public Paint getFillColor() {
+        if(shape instanceof Shape)
+            return ((Shape)shape).getFill();
+        else return Color.BLACK;
+    }
+    @Override
+    public Paint getStrokeColor() {
+        if(shape instanceof Shape)
+            return ((Shape)shape).getStroke();
+        else return Color.BLACK;
+    }
+    @Override
+    public double getStrokeWidth() {
+        if(shape instanceof Shape){
+            return ((Shape)shape).getStrokeWidth();
+        } else return 0;
+    }
+    @Override
+    public double[] getStrokeDashArray() {
+        if(shape instanceof Shape){
+            ObservableList<Double> l= ((Shape)shape).getStrokeDashArray();
+            double [] a = new double[l.size()];
+            for(int i = 0; i < a.length; i++)
+                a[i] = l.get(i);
+            return a;
+        } else return new double[0];        
+    }
+    @Override
+    public Font getFont() {
+        if(shape instanceof Text){
+           return ((Text)shape).getFont();
+        }
+        else return Font.getDefault();//System default
+    }
+
+    @Override
+    public void setText(String value) {
+        if(shape instanceof Text){
+           ((Text)shape).setText(value);
+        }
+    }
+
+    @Override
+    public String getText() {
+        if(shape instanceof Text){
+           return ((Text)shape).getText();
+        }
+        else return "";
+    }
+}
+
